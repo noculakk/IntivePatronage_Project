54f46ac5c44dd2aef41e84c9a6d6e3133e48c082
Kornelia Noculak <noculakkx@gmail.com>
2021-12-04 16:35:14 +0100

    Implemented method countWords in WordCounter class

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -6,7 +6,20 @@ import java.util.HashMap;
 public class WordCounter {
 
     public static boolean checkSize(String filePath, long maxFileSize) throws IOException {
-            long sizeOfFile = Files.size(Path.of(filePath));
-            return sizeOfFile < maxFileSize;
+        long sizeOfFile = Files.size(Path.of(filePath));
+        return sizeOfFile < maxFileSize;
+    }
+
+    public static HashMap<String, Integer> countWords(String filePath) throws IOException {
+        HashMap<String, Integer> wordsMap = new HashMap<>();
+
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        for (String line; (line = reader.readLine()) != null; ) {
+            for (String w : line.trim().split("\\s+")) {
+                w = w.replaceAll("[^a-zA-Z]", "");
+                wordsMap.merge(w, 1, Integer::sum);
+            }
+        }
+        return wordsMap;
     }
 }
